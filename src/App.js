9a46b83bf51c47025322bb95dc48166f340952e9
Onyedikachi <onyedikachinwosu@rocketmail.com>
2023-02-09 16:16:25 +0100

    update app component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import Header from './components/header/header'
+import Nav from './components/nav/nav'
+import About from './components/about/about'
+import Contact from './components/contact/contact'
+import Experience from './components/experience/experience'
+import Portfolio from './components/portfolio/portfolio'
+import React, { useEffect, useState} from 'react'
+
+
+import AOS from 'aos'
+import "aos/dist/aos.css"
+import Footer from './components/Footer'
+
+const App = () => {
+  const [loading, setLoading] = useState(true)
+ const loader= document.getElementById('container')
+
+  if(loader) {
+    setTimeout(() => {
+      loader.style.display= 'none'
+      setLoading(false)
+    },2000)}
+  
+
+  useEffect(() => {
+    AOS.init({duration: 1500})
+    AOS.refresh()
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   !loading && (
+      <div>
+       <Nav />
+      <Header />
+      <About />
+      <Experience />
+      <Portfolio />
+      <Contact />
+      <Footer />
+    </div> 
+   )
+  )
 }
 
-export default App;
+export default App
