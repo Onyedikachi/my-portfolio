8334f3c582bcfc457e2fb197a02eadb1881c5396
Onyedikachi <onyedikachinwosu@rocketmail.com>
2023-02-09 16:28:43 +0100

    fix header page

---
+++ src/components/header/header.js
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from 'react'
+import './header.css'
+import CTA from '../CTA'
+import Socials from '../Socials'
+
+const Header = () => {
+ 
+    
+
+
+  return (
+  <header>
+   
+      <div className="container header_container">
+        <div className="hero-text" data-aos='zoom-out' data-aos-delay='1000'>
+          <h4>Hello I'm</h4>
+          <h1>Nwosu Onyedikachi</h1>
+          <p className='light'>A Software Engineer</p>
+          <CTA/>
+         </div>
+         <Socials />
+      </div>
+  
+    </header>
+  )
+}
+
+export default Header
