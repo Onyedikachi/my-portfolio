58f911c565d18e5ad4b6ef9962ee45b0831b6422
Onyedikachi <onyedikachinwosu@rocketmail.com>
2023-02-14 22:08:57 +0100

    rename Project to Projects in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,7 +8,7 @@ function Footer() {
        <ul>
          <li><a href="#">Home</a></li>
          <li><a href="#about">About</a></li>
-         <li><a href="#portfolio">Project</a></li>
+         <li><a href="#portfolio">Projects</a></li>
          <li><a href="#experience">Skills</a></li>
          {/* <li><a href="#services">Services</a></li> */}
          <li><a href="#contact">Contact</a></li>
