f9c97f4a7ae3142a58fa26b4627fb35b41e002f6
Onyedikachi <onyedikachinwosu@rocketmail.com>
2023-02-20 09:58:20 +0100

    add alert and spinners; apply email service lambda

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -1,5 +1,7 @@
 import React, {useState} from 'react'
 import axios from 'axios'
+import { useAlert } from 'react-alert'
+import {HashLoader} from 'react-spinners';
 
 import './contact.css'
 import {MdOutlineEmail} from 'react-icons/md'
@@ -15,36 +17,53 @@ const Contact = () => {
   const [email, setEmail] = useState('');
   const [name, setName] = useState('');
   const [phone, setPhone] = useState('');
+
+  const resetForm = () => {
+    setMessage('');
+    setEmail('');
+    setName('');
+    setPhone('');
+  };
+
+  const alert = useAlert();
+
   const handleSubmit = async(e) => {
     e.preventDefault();
     setLoading(true);
 
-    const res = await axios({
-      url: lambdaApi,
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      method: 'post',
-      data: {
-        function: 'email-service',
-        type: 'contact_mail',
-        from: 'contactus@onyedikachinwosu.com.ng',
-        message,
-        name,
-        email,
-        phone,
-        emailSubject: "Enquiries"
-      }
-    });
+    try {
+      await axios({
+        url: lambdaApi,
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        method: 'post',
+        data: {
+          function: 'email-service',
+          type: 'contact_mail',
+          from: 'contactus@onyedikachinwosu.com.ng',
+          message,
+          name,
+          email,
+          phone,
+          emailSubject: "Enquiries"
+        }
+      });
+
+      alert.show('Email sent', {
+        onClose: () =>{
+          resetForm();
+        }
+      });
+    } catch (error) {
+        alert.error(error.message);
+    }
 
     setLoading(false);
-
-    console.log(res.data);
 
   }
   return ( 
     <>
-      {loading && <div>sending email</div>}
        <section id='contact'>
        <h2 data-aos='fade-up'>Get in touch</h2>
        <p data-aos='fade-down'> Do you need help with your projects? 
@@ -68,21 +87,29 @@ const Contact = () => {
            </article>
          </div>
          {/* end if contact options */}
+         
          <form onSubmit={handleSubmit} data-aos='zoom-in-right' >
            <input 
-             onChange={(e) => setName(e.target.value)}  
+             onChange={(e) => setName(e.target.value)}  value={name}
              type='text' name='name' placeholder='Your Full Name' required />
            <input 
-             onChange={(e) => setEmail(e.target.value) }
+             onChange={(e) => setEmail(e.target.value) } value={email}
              type='email' name='email' placeholder='Your Email' required  />
            <input 
-             onChange={ (e) => setPhone(e.target.value)  }
+             onChange={ (e) => setPhone(e.target.value)  } value={phone}
              type='phone' name='phone' placeholder='Your Phone Number' />
            <textarea 
-             onChange={ (e) => setMessage(e.target.value) }
+             onChange={ (e) => setMessage(e.target.value) } value={message}
              name='message' rows='7' placeholder='Your Message' required></textarea>
-           <button type='submit' className='btn btn-primary'>Send message</button>
+           <button type='submit' className='btn btn-primary'>
+              Send message
+            </button>
+            <HashLoader
+                color={'#000'} 
+                loading={loading}
+              />
          </form>
+
        </div>
      </section>
     </>
